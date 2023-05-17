a6670b53cd431a07e6cd88bbd25e0cb2ee847211
tatarenkov-r-v <flexomj@gmail.com>
2023-05-17 15:36:24 +0400

    Поправил импорты, пробелы. Исправлен общий путь для api добавлен "path('', include(router_v1.urls))".

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,25 +1,18 @@
 from django.urls import include, path
 from rest_framework import routers
-from django.urls import path, include
-from users.views import UsersView, AuthGetTokenView, AuthSignUpView
+from users.views import AuthGetTokenView, AuthSignUpView, UsersView
 
-from .views import (
-    CategoryViewSet,
-    CommentViewSet,
-    GenreViewSet,
-    ReviewViewSet,
-    TitleViewSet,
-)
+from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                    ReviewViewSet, TitleViewSet)
+
 
 class NoPutRouter(routers.DefaultRouter):
     """Класс роутер, отключающий PUT запросы."""
-
     def get_method_map(self, viewset, method_map):
         bound_methods = super().get_method_map(viewset, method_map)
         if 'put' in bound_methods.keys():
             del bound_methods['put']
         return bound_methods
-
 
 router_v1 = NoPutRouter()
 router_v1.register('categories', CategoryViewSet, basename='categories')
@@ -34,15 +27,11 @@ router_v1.register(
     basename='comments',
 )
 
-
 app_name = 'api'
 
-urlpatterns = []
-
-# Эндпоинты работы с пользователями авторизации и регистрации
-urlpatterns += [
+urlpatterns = [
+    path('', include(router_v1.urls)),
     path('users/', UsersView.as_view()),
     path('auth/token/', AuthGetTokenView.as_view()),
     path('auth/signup/', AuthSignUpView.as_view()),
 ]
-
