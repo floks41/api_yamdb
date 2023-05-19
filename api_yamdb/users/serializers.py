ec5164a4f65afbb89971b6ba5ef4879963a93bac
chuzhmarov <choa4@yandex.ru>
2023-05-19 14:30:00 +0300

    SignUpSerializer

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -19,6 +19,23 @@ class UserSerializer(serializers.ModelSerializer):
                 'Username. 150 characters or fewer. '
                 'Letters, digits and @/./+/-/_ only.')
         return value
+    
+    # def validate_email(self, value):
+    #     if self.instance:
+    #         if value != self.instance.email:
+    #             raise serializers.ValidationError({'email':'Wrong email.'})
+    #     else:
+    #         if self.Meta.model.objects.filter(email=value).exists():
+    #             raise serializers.ValidationError(
+    #                 'User with that email exists already.')
+    #     return value
+    def validate(self, data):
+        if not self.instance:
+            if self.Meta.model.objects.filter(username=data.get('username', None)).exists():
+                self.instance = self.Meta.model.objects.filter(username=data.get('username', None))
+                self.is_valid()
+        return data
+            
 
 
 class UserMeSerializer(UserSerializer):
@@ -30,5 +47,66 @@ class UserMeSerializer(UserSerializer):
 
 
 class UserAuthSignUpSerializer(UserMeSerializer):
+    
     class Meta(UserMeSerializer.Meta):
         fields = ('username', 'email')
+
+
+class SignUpSerializer(UserSerializer):
+    username = serializers.CharField(required=True, max_length=150)
+    email = serializers.EmailField(required=True, max_length=254)
+    corresponding_object: object
+    is_exist_checked = False
+    has_object = False
+    
+    def check_object(self, username=None, email=None):
+        if self.is_exist_checked:
+            return self.has_object
+        username = self.initial_data.get('username')
+        if username and self.Meta.model.objects.filter(username=username).exists():
+            # self.corresponding_object = self.Meta.model.objects.get(username=username)
+            # self.instance = self.corresponding_object
+            self.instance = self.Meta.model.objects.get(username=username)
+            self.has_object = True
+            self.is_exist_checked = True
+        return self.has_object
+
+    def validate_username(self, value):
+        if value.lower() == 'me':
+            raise serializers.ValidationError(
+                'User cannot have username \'me\'.')
+        pattern = re.compile(r'^[\w.@+-]+\Z')
+        if not pattern.match(value):
+            raise serializers.ValidationError(
+                'Username. 150 characters or fewer. '
+                'Letters, digits and @/./+/-/_ only.')
+        return value
+    
+    def validate_email(self, value):
+        if self.check_object() or self.instance:
+            if value != self.instance.email:
+                raise serializers.ValidationError({'email':'Wrong email1.'})
+        else:
+            if self.Meta.model.objects.filter(email=self.initial_data.get('email')).exists():
+                    raise serializers.ValidationError(
+                        'User with that eamil exists already.')
+        return value
+
+
+        # if self.instance:
+        #     if value != self.instance.email:
+        #         raise serializers.ValidationError({'email':'Wrong email1.'})
+        # else:
+        #     if self.check_object():
+        #         if self.corresponding_object.email != value:
+        #             raise serializers.ValidationError(
+        #                 'Wrong email2.')
+        #     else:
+        #         if self.Meta.model.objects.filter(email=self.initial_data.get('email')).exists():
+        #             raise serializers.ValidationError(
+        #                 'User with that eamil exists already.')
+        # return value
+    
+    class Meta(UserSerializer.Meta):
+        fields = ('username', 'email')
+   
--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -5,7 +5,7 @@ from api.permissions import IsAdmin
 from django.contrib.auth.base_user import BaseUserManager
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
-from rest_framework import filters, status, viewsets
+from rest_framework import filters, status, viewsets, mixins
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -14,7 +14,7 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from users.models import User
 from users.serializers import (UserAuthSignUpSerializer, UserMeSerializer,
-                               UserSerializer)
+                               UserSerializer, SignUpSerializer)
 
 
 class AuthGetTokenView(APIView):
@@ -55,48 +55,61 @@ class AuthSignUpView(APIView):
     permission_classes = (AllowAny,)
 
     def post(self, request):
-        email = request.data.get('email', '')
-        username = request.data.get('username', '')
+        # email = request.data.get('email', '')
+        # username = request.data.get('username', '')
 
-        # Проверка существования переденного username.
-        if User.objects.filter(username=username).exists():
-            user = User.objects.get(username=username)
-            # Проверка существования переденного email.
-            if user.email != email:
-                return Response('Wrong email.',
-                                status=status.HTTP_400_BAD_REQUEST)
-            # Для существующего пользователя используем сериализатор
-            # с отвязанными от модели полями,
-            # иначе данные не пройдут валидацию в сериализаторе.
-            serializer = UserAuthSignUpSerializer(data=request.data)
-        else:
-            # Для нового пользователя используем стандартный сериализатор
-            # без дополнительных проверок.
-            serializer = UserSerializer(data=request.data)
-
-        if not serializer.is_valid():
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
-
-        # Сохранение/получение объекта пользователя.
-        email = serializer.validated_data.get('email')
-        username = serializer.validated_data.get('username')
-        user, created = User.objects.get_or_create(
-            username=username, email=email)
-
-        # Подготовка, сохранение confirmation_code.
-        confirmation_code = BaseUserManager.make_random_password(self)
-        user.confirmation_code = confirmation_code
-        user.save()
-
-        # Отправка Confirmation code по email.
-        send_mail(subject="Confirmation code",
-                  message=confirmation_code,
+        serializer = SignUpSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save(confirmation_code=BaseUserManager.make_random_password(None))
+            # user.confirmation_code = BaseUserManager.make_random_password(self)
+            # user.save
+            send_mail(subject="Confirmation code",
+                  message=user.confirmation_code,
                   from_email="admin@yamdb.fun",
                   recipient_list=[user.email, ])
+            return Response(data=serializer.validated_data,
+                            status=status.HTTP_200_OK)
+        return Response(serializer.errors,
+                        
+                        status=status.HTTP_400_BAD_REQUEST)
+        
 
-        return Response(data=serializer.validated_data,
-                        status=status.HTTP_200_OK)
+        
+
+        # Проверка существования переденного username.
+        # if User.objects.filter(username=username).exists():
+        #     user = User.objects.get(username=username)
+        #     serializer = UserAuthSignUpSerializer(instance=user, data=request.data)
+        # else:
+        #     serializer = UserSerializer(data=request.data)
+
+        # if not serializer.is_valid():
+        #     return Response(serializer.errors,
+        #                     status=status.HTTP_400_BAD_REQUEST)
+        
+
+        # Сохранение/получение объекта пользователя.
+        # email = serializer.validated_data.get('email')
+        # username = serializer.validated_data.get('username')
+        # user, created = User.objects.get_or_create(
+        #     username=serializer.validated_data.get('username'), 
+        #     email=serializer.validated_data.get('email'))
+        
+        # user, created = User.objects.get_or_create(**serializer.validated_data)
+
+        # Подготовка, сохранение confirmation_code.
+        # confirmation_code = BaseUserManager.make_random_password(self)
+        # user.confirmation_code = confirmation_code
+        # user.save()
+
+        # # Отправка Confirmation code по email.
+        # send_mail(subject="Confirmation code",
+        #           message=user.confirmation_code,
+        #           from_email="admin@yamdb.fun",
+        #           recipient_list=[user.email, ])
+
+        # return Response(data=serializer.validated_data,
+        #                 status=status.HTTP_200_OK)
 
 
 class UsersViewSet(viewsets.ModelViewSet):
@@ -107,7 +120,7 @@ class UsersViewSet(viewsets.ModelViewSet):
     pagination_class = PageNumberPagination
     filter_backends = (filters.SearchFilter,)
     search_fields = ('username',)
-
+        
     @action(detail=False, methods=['GET', 'PATCH'], name='User profile',
             permission_classes=(IsAuthenticated,))
     def me(self, request, *args, **kwargs):
