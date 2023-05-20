d30e4e251827f849ce614a10ddf47a8a62815bcf
Roman Tatarenkov <flexomj@gmail.com>
2023-05-20 22:26:09 +0400

    Косметические правки.

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls', namespace='api')),
