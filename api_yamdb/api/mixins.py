afc0f975654d978fae02e79e645808357767ef4d
chuzhmarov <choa4@yandex.ru>
2023-05-22 00:46:57 +0300

    usersme fix3

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -8,3 +8,12 @@ class CreateDestroyListViewSet(
     viewsets.GenericViewSet
 ):
     pass
+
+
+class NotPutViewSet(viewsets.GenericViewSet):
+
+def update(self, request, *args, **kwargs):
+    """PUT-method is prohibited."""
+    if request.method == 'PUT':
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    return super().update(request, *args, **kwargs)
